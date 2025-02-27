f38e148acd24729f3043338eccd6868f3268175c
“Calixto” <l19070031@huetamo.tecnm.mx>
2025-02-26 22:48:20 -0600

    Importando variables BD

--- src/views/model/db.js
+++ src/views/model/db.js
@@ -1,8 +1,9 @@
 const { createClient } = require('@libsql/client');
+const { TURSO_AUTH_TOKEN, TURSO_DATABASE_URL } = require('../../config/config.js');
 
 const client = createClient({
-    url: process.env.TURSO_DATABASE_URL,
-    authToken: process.env.TURSO_AUTH_TOKEN
+    url: TURSO_DATABASE_URL,
+    authToken: TURSO_AUTH_TOKEN
 });
 
 module.exports = {
